c7a70bd485b8e79f59c6597d0eb7df564e1104be
Keli06001 <keli06001@utb.vaxjo.se>
2023-11-27 14:27:53 +0100

    Add project files.

---
+++ Inlämningsuppgift 5.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Inlämningsuppgift5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[5];
+            Console.WriteLine("Skriv in 5 namn");
+            for (int i = 0; i < 5; i++)
+            {
+                namn[i] = Console.ReadLine();
+            }
+            for(int i = 0;i < 5; i++) 
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
